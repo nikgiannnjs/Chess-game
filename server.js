48d1fe326be60c3c26a4f6393ba12effdac2faea
nikos <nikgiannsm@gmail.com>
2024-07-24 16:22:41 +0300

    Set up EJS view engine

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-dotenv.config();
 const sqlDB = require("./config/database");
 const redisClient = require("./config/redis");
+const path = require("path");
+dotenv.config();
 
 const port = process.env.PORT;
 
+//MySQL connection
 sqlDB.connect((err) => {
   if (err) {
     console.log(err);
@@ -16,8 +18,16 @@ sqlDB.connect((err) => {
   }
 });
 
+//EJS setup
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Server connection
 app.get("/", (req, res) => {
-  res.send("Hello for server.");
+  res.render("index");
 });
 
 app.listen(port, () => {
